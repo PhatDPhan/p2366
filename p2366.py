63cd712c72c539b9e1d833df1f74e0df7889a8a7
PhatDPhan <33110554+PhatDPhan@users.noreply.github.com>
2018-03-20 21:54:14 -0400

    commit

--- p2366.py
+++ p2366.py
@@ -1,26 +1,28 @@
-# Phat Phan, Kevin Sie
+# Phat Phan, Kevin Sie, Daniel Rodriguez
 # La Salle University, CSC366, Spring 2018
 # Project 2: Infix to Postfix
-# Dowload link: https://www.python.org/downloads/
+# Download link: https://www.python.org/downloads/
 # The project is written on Python 3 version
 
 import sys
-import string
 
-# re class is the regex class that help dealing with variables that contain 2 or more letter for example a1, a2, 9562 or omg
+# re - regex class: helps to deal with variables that contain 2 or more letter for example a1, a2, 9562 or omg.
 import re
-# Kevin provided two buid-in methods of python that can handle better then regex. they are isalnum() and isdigit()
-# However, in the loop to ask users if they want to do another expression, it's simplier with regex
 
-# The advantage of python 3 is that when it breaks the input into tokens. each token will automatically be seperated by blank space.
-# for example A1 + 6 will give A1 6 +
 
-# The diadvantage is when users input without anyspace. there will be error. For example, a+5, a+ 5 because when
-# it splits the input, it will see a+5 or a+ as a whole token
+# Daniel provided two build-in methods of Python that can handle better than regex. They are isalnum() and isdigit().
+# However, in the loop to ask users if they want to do another expression, it's simpler with regex.
+
+# The advantage of Python 3 is that when it breaks the string input into tokens,
+# each token will automatically be separated by blank space.
+# For example A1 + 6 will give A1 6 +
+
+# The disadvantage is when users input without any space, there will be an error.
+# For example, a+5, a+ 5 because when it splits the input, it will see a+5 or a+ as a whole token
 # Kevin provided fix_space method to handle tokens with or without space
 
-# Define class stack that has 5 methods. we will use push and pop and peak to work on each token
-# Their others 4 are used for checking
+# Define a class (stack) - that has 5 methods. We will use push and pop and peek to work on each token
+# The other 4 are used for checking
 class Stack:
     def __init__(self):
         self.items = []
@@ -33,14 +35,16 @@ class Stack:
 
     def pop(self):
         return self.items.pop()
-    # go back 1 position in the stack
+
+    # Go back 1 position in the stack
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
-       
-# method to convert infix to postfix    
+
+
+# Method to convert infix to postfix
 def infixToPostfix(userInput):
     prec = {}
     prec["^"] = 3
@@ -50,22 +54,24 @@ def infixToPostfix(userInput):
     prec["+"] = 1
     prec["-"] = 1
     prec["("] = 0
-    
-    #create a stack to hold the left parentthesis and all operations +-*/%^ and variables
+
+    # Create a stack to hold the left parenthesis and all operations +-*/%^ and variables
     operation = Stack()
-	
-    #create the array that holds postfix output
+
+    # Create the array that holds postfix output
     postfixList = []
-    
-    #array to hold breaking down input as tokens
+
+    # Array to hold breaking down input as tokens
     tokenList = fix_spaces(userInput).split()
 
-    # loop throught each token
-    # check if each token is a variable, number, letter, or left-right parentthesis
-    # when it sees a  right parentthesis meaning the end of an expression(it may a small expression inside a bigger epression)
-    # it will pop  everything in the operation stack to the postfixList
+    # Loop through each token
+    # Check if each token is a variable, number, letter, or left-right parenthesis
+    # When it sees a  right parenthesis meaning the end of an expression
+    # (it may a small expression inside a bigger expression)
+    # It will pop everything in the operation stack to the postfixList
+
     for token in tokenList:
-        # if re.search('[a-zA-Z]', token) or token in str(list(range(10000))): old manualy check
+        # If re.search('[a-zA-Z]', token) or token in str(list(range(10000))): old manually check
         if token.isalnum():
             postfixList.append(token)
         elif token == '(':
@@ -79,34 +85,36 @@ def infixToPostfix(userInput):
             while (not operation.isEmpty()) and (prec[operation.peek()] >= prec[token]):
                 postfixList.append(operation.pop())
             operation.push(token)
-    #pop out everything in the operation stack to the postfixList
+    # Pop out everything in the operation stack to the postfixList
     while not operation.isEmpty():
         postfixList.append(operation.pop())
     return " ".join(postfixList)
 
-# Postfix evaluation method, if the expressions contain just number
-# it will calculate output
+
+# Postfix evaluation method, if the expressions contain only number
+# It will calculate output
 def postfixEval(postfixExpr):
-    # create stack to hold all arimitic operations and number
+    # Create stack to hold all arithmetic operations and number
     operandStack = Stack()
     # postfixExpr is the result of inFixtoPostfix method
     tokenList = postfixExpr.split()
 
     # Loop through the list
-    # it checks 3 tokens from left to right at a time
-    # for example 6 3 + 6 - meaning 6 + 3 = 9 then 9 - 6 = 3
+    # It checks 3 tokens from left to right at a time
+    # For example 6 3 + 6 - meaning 6 + 3 = 9 then 9 - 6 = 3
     for token in tokenList:
-        # if token in str(list(range(19998))): old manualy check
+        # If token in str(list(range(19998))): old manually check
         if token.isdigit():
             operandStack.push(int(token))
         else:
             operand2 = operandStack.pop()
             operand1 = operandStack.pop()
-            result = doMath(token,operand1,operand2)
+            result = doMath(token, operand1, operand2)
             operandStack.push(result)
     return operandStack.pop()
 
-# method to calculate the posfix
+
+# Method to calculate the postfix
 def doMath(op, op1, op2):
     if op == "*":
         return op1 * op2
@@ -138,19 +146,19 @@ def fix_spaces(string):
 def main():
     # A loop to ask user if they want to continue or not
     while True:
-# Read user input
+        # Read user input
         expression = input('Please enter your expression!\n')
 
-# Check if input contains just characters, number, or both
-# If input contain just characters or both characters and number,
-# then use infixtoposstfix method without evaluation
-# If the input contain just number then evaluate it.
+        # Check if input contains just characters, number, or both
+        # If input contain just characters or both characters and number,
+        # then use infixtoposstfix method without evaluation
+        # If the input contain only numbers then evaluate it.
 
-        c=True
+        c = True
         check = expression.split()
         for char in check:
             if re.search('[a-zA-Z]', char):
-                c=False
+                c = False
 
         if c:
             print("This is the Postfix:")
@@ -161,13 +169,14 @@ def main():
             print("This is the Postfix:")
             print(infixToPostfix(expression))
 
-# Ask user if they want to continue        
+        # Ask user if they want to continue
         again = input("\nDo you want to continue?  Y/N\n").lower()
         if again == "y":
             continue
         else:
-            break 
+            break
     sys.exit()
 
-if  __name__ =='__main__':
+
+if __name__ == '__main__':
     main()
